d409d029663cb486490c72e07445d2320ebe4c4d
christal <christal.b.camillo@gmail.com>
2022-04-05 12:46:39 -0300

    comeco

---
+++ simCity.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+	
+}
+
+public static float calcImposto (float salario){
+	float imposto = 0;
+	for(2000 < salario < 3000){
+		imposto = salario % 
+	}
+
+}
